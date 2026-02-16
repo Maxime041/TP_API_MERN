963dd39c0861e06db0a681f92dc003a8b75fc6d9
Maxime04 <maxime.danino04@gmail.com>
2026-02-16 14:25:15 +0100

    TP 3 : Gestion des Erreurs (Try/Catch)

--- tp3-typescript/src/index.ts
+++ tp3-typescript/src/index.ts
@@ -3,9 +3,21 @@ import { mockUsers, fetchUtilisateurs } from "./service";
 console.log(`Nombre d'utilisateurs : ${mockUsers.length}`);
 
 // Une fonction qui utilise 'await' DOIT être marquée 'async'
-async function main() {
+/*async function main() {
     console.log("Chargement des données...");
     const users = await fetchUtilisateurs();
     console.log("Utilisateurs reçus :", users);
+}*/
+
+async function main() {
+try {
+        console.log("Tentative de connexion...");
+        const users = await fetchUtilisateurs();
+        console.log(users);
+    } 
+    catch (error) {
+        console.log("error :", error);
+    }
 }
+
 main();
--- tp3-typescript/src/service.ts
+++ tp3-typescript/src/service.ts
@@ -10,12 +10,26 @@ export const mockUsers: Utilisateur[] = [
 ];
 
 // Notez le type de retour : on promet de renvoyer un tableau d'utilisateurs plus tard
-export function fetchUtilisateurs(): Promise<Utilisateur[]> {
+/*export function fetchUtilisateurs(): Promise<Utilisateur[]> {
     return new Promise((resolve) => {
         // setTimeout simule la lenteur du réseau (2 secondes)
         setTimeout(() => {
             console.log("... Données récupérées !");
             resolve(mockUsers); // La promesse est tenue, on envoie les données
         }, 2000);
+    });
+}*/
+
+export function fetchUtilisateurs(): Promise<Utilisateur[]> {
+    return new Promise((resolve, reject) => { // Notez l'ajout de 'reject'
+        setTimeout(() => {
+            const success = Math.random() > 0.5; // 1 chance sur 2
+            if (success) {
+            resolve(mockUsers);
+            } else {
+            // TODO: Appelez reject() avec un message d'erreur (string)
+            reject("Erreur lors de la récupération des utilisateurs");
+            }
+        }, 1000);
     });
 }
